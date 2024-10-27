2e1c4677695c99c2f4f56ef050833bbdd9268705
Keks <keks@htmlacademy.ru>
2024-10-27 19:53:06 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/getMiniatures.js
@@ -0,0 +1,31 @@
+import { descriptionPhoto } from './createArrayMiniatures.js';
+
+const container = document.querySelector('.pictures');
+const template = document.querySelector('#picture').content.querySelector('.picture');
+
+// Наполняет шаблон данными для миниатюр
+const createMiniatureElement = (photo) => {
+  const miniatureElement = template.cloneNode(true);
+
+  miniatureElement.href = photo.url;
+  miniatureElement.dataset.id = photo.id;
+
+  const image = miniatureElement.querySelector('.picture__img');
+
+  image.src = photo.url;
+  image.alt = photo.description;
+  miniatureElement.querySelector('.picture__likes').textContent = photo.likes;
+  miniatureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+
+  return miniatureElement;
+};
+
+const containerFragment = document.createDocumentFragment();
+
+// Добавляет миниатюры в разметку
+descriptionPhoto.forEach((photo) => {
+  const miniatureElement = createMiniatureElement(photo);
+  containerFragment.appendChild(miniatureElement);
+});
+
+container.appendChild(containerFragment);
