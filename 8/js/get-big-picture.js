d250e276b1928721dd24f7f1a0e3abf65973cf2f
Keks <keks@htmlacademy.ru>
2024-11-02 19:46:42 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/get-big-picture.js
@@ -0,0 +1,55 @@
+import './get-miniatures.js';
+import { isEscapeKey } from './util.js';
+import { descriptionPhoto } from './create-array-miniatures.js';
+import { openComments, clearComments } from './open-comments.js';
+
+const bigPictureElement = document.querySelector('.big-picture');
+const bigPictureImg = bigPictureElement.querySelector('.big-picture__img img');
+const likesCount = bigPictureElement.querySelector('.likes-count');
+const caption = bigPictureElement.querySelector('.social__caption');
+
+const buttonCancel = document.querySelector('.big-picture__cancel');
+
+// Обработчик нажатия на крестик
+const onCloseBigPictureClick = (evt) => {
+  evt.preventDefault();
+  // eslint-disable-next-line no-use-before-define
+  closeBigPicture();
+};
+// Обработчик нажатия клавиши ESC
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    // eslint-disable-next-line no-use-before-define
+    closeBigPicture();
+  }
+};
+
+// Функция для закрытия большой картинки
+const closeBigPicture = () => {
+  clearComments();
+  bigPictureElement.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  buttonCancel.removeEventListener('click', closeBigPicture);
+};
+
+// Функция для открытия большой картинки
+const openBigPicture = (id) => {
+  const currentPhoto = descriptionPhoto.find((photo) => photo.id === Number(id));
+
+  bigPictureImg.src = currentPhoto.url;
+  bigPictureImg.alt = currentPhoto.description;
+  caption.textContent = currentPhoto.description;
+  likesCount.textContent = currentPhoto.likes;
+
+  openComments(currentPhoto.comments);
+
+  bigPictureElement.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  buttonCancel.addEventListener('click', onCloseBigPictureClick);
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+export { openBigPicture };
---
+++ 8/js/main.js
@@ -0,0 +1,1 @@
+import './open-big-picture.js';
