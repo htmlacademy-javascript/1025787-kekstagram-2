bedf5383c9acf690bfc782e8064dbccd072e4622
Ekaterina-Kozlova28 <ekaterinakozlova0@gmail.com>
2024-11-13 22:10:45 +0300

    Проверяет валидность хештегов и их количество

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+import '../vendor/pristine/pristine.min.js';
 import { descriptionPhoto } from './create-array-miniatures.js';
 import { renderCards } from './get-miniatures.js';
-import { photoUpload } from './photo-upload-form.js';
+// import { photoUpload } from './photo-upload-form.js';
+import './photo-upload-form.js';
 
 renderCards(descriptionPhoto);
-photoUpload();
+// photoUpload();
--- js/photo-upload-form.js
+++ js/photo-upload-form.js
@@ -1,8 +1,18 @@
 import { isEscapeKey } from './util.js';
 
+const imgForm = document.querySelector('.img-upload__form');
 const imgUpload = document.querySelector('.img-upload__input');
 const photoEditing = document.querySelector('.img-upload__overlay');
 const cancel = document.querySelector('.img-upload__cancel');
+
+const pristine = new Pristine(imgForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'img-upload__field-wrapper--invalid',
+  successClass: 'img-upload__field-wrapper--valid',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'div',
+  errorTextClass: 'img-upload__field-wrapper--error'
+});
 
 // Обработчик нажатия на крестик
 function onCloseForm(evt) {
@@ -35,10 +45,47 @@ const hideForm = () => {
   cancel.removeEventListener('click', onCloseForm);
   document.removeEventListener('keydown', onDocumentKeydown);
   imgUpload.removeEventListener('change', shownForm);
+  imgUpload.value = '';
 };
+
+const validateHashtag = (value) => {
+  const hashtags = value.split(' ');
+  let element;
+  hashtags.forEach((hashtag) => {
+    element = /^#[a-zа-я0-9]{1,19}$/i.test(hashtag);
+  });
+  return element === true && hashtags.length <= 5;
+};
+
+const getErrorMessage = (value) => {
+  const hashtags = value.split(' ');
+  let element;
+  hashtags.forEach((hashtag) => {
+    element = /^#[a-zа-я0-9]{1,19}$/i.test(hashtag);
+  });
+  if (element === false) {
+    return 'Неправильный хэштег';
+  }
+  if (hashtags.length > 5) {
+    return 'Превышено максимальное количество хэштегов - 5';
+  }
+};
+
+pristine.addValidator(
+  imgForm.querySelector('.text__hashtags'),
+  validateHashtag,
+  getErrorMessage
+);
 
 const photoUpload = () => {
   imgUpload.addEventListener('change', shownForm);
+
+  imgForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    pristine.validate();
+  });
 };
 
 export { photoUpload };
+
+photoUpload();
