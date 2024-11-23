da2fe3c1748139d43c18cfa8e86da2ab8f103411
Ekaterina-Kozlova28 <ekaterinakozlova0@gmail.com>
2024-11-23 18:06:29 +0300

    Загружает данные с сервера и отрисовывает их

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-import { descriptionPhoto } from './create-array-miniatures.js';
+// Больше не надо создавать миниатюры из списка фотографий и описаний, т.к. беру их с сервера:
+// import { descriptionPhoto } from './create-array-miniatures.js';
+
 import { renderCards } from './get-miniatures.js';
 import { photoUpload } from './photo-upload-form.js';
 
-renderCards(descriptionPhoto);
+// Функцию использую при получении данных с сервера:
+// renderCards(descriptionPhoto);
+
+// Получаю данные с сервера:
+fetch('https://31.javascript.htmlacademy.pro/kekstagram/data')
+  .then((response) => response.json())
+  .then((miniatures) => {
+    renderCards(miniatures);
+  });
+
 photoUpload();
