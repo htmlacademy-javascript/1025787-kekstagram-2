64e7bb13fe3c87a9d0fd9c1ab5cc0b66526a223e
Ekaterina-Kozlova28 <ekaterinakozlova0@gmail.com>
2024-11-23 19:38:38 +0300

    Убирает ошибку в адресе сервера

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 const getData = () => fetch(
-  'https://31.javascript.htmlacadem.pro/kekstagram/data')
+  'https://31.javascript.htmlacademy.pro/kekstagram/data')
   .then((response) => response.json());
 
 export { getData };
