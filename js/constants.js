212431e57fa2dde7654f4e96d7ba58f6fadada4f
Ekaterina-Kozlova28 <ekaterinakozlova0@gmail.com>
2024-12-07 18:16:52 +0300

    Добавляет фильтрацию изображений

--- js/constants.js
+++ js/constants.js
@@ -68,3 +68,14 @@ export const PopupTypes = {
   SUCCESS: 'success',
   ERROR: 'error'
 };
+
+export const FILTERS = {
+  DEFAULT: 'filter-default',
+  RANDOM: 'filter-random',
+  DISCUSSED: 'filter-discussed'
+};
+
+export const SORTFUNC = {
+  RANDOM: () => 0.5 - Math.random(),
+  DISCUSSED: (a, b) => b.comments.length - a.comments.length
+};
